8e2585d8648086a88eae36189e1e913a3ff057fe
k.vasin <k.vasin@machineheads.ru>
2025-06-04 02:04:38 +0700

    save

--- local/markup/vite/vite.config.ts
+++ local/markup/vite/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     build: {
+        // outDir: '../local/templates/main/dist',
         manifest: true,
     },
     server: {
