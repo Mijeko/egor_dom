45ec3c6d07c4fc660bd0031d3d5ac270b91aeb68
konstantin <k.vasin@craftpromotion.ru>
2025-06-09 15:44:42 +0700

    save

--- local/markup/dom-egor/src/components/Developers.vue
+++ local/markup/dom-egor/src/components/Developers.vue
@@ -15,7 +15,14 @@ export default defineComponent({
       Застройщики
 
       <v-card v-for="developer in developers">
-        <v-card-text>{{ developer.name }}</v-card-text>
+        <v-row>
+          <v-col md="3">
+            <v-img :src="developer.picture"></v-img>
+          </v-col>
+          <v-col md="9">
+            <v-card-text>{{ developer.name }}</v-card-text>
+          </v-col>
+        </v-row>
       </v-card>
     </v-container>
   </v-app>
