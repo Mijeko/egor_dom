ec1ab3c3484994d83a5069c646d8df7b334c795b
k.vasin <k.vasin@machineheads.ru>
2025-07-20 22:59:47 +0700

    save

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,10 +2,10 @@
 
 define('NEED_AUTH', $_SERVER['SERVER_NAME'] === 'abn.dev-kvsn.ru');
 
-function demoAgent()
+function testAgent()
 {
 	\Bitrix\Main\Diag\Debug::dumpToFile('demo_agent', '', '__agent.log');
-	return 'demoAgent();';
+	return "testAgent();";
 }
 
 if(\Bitrix\Main\Loader::includeModule('craft.core'))
