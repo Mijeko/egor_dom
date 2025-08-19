92a0648d6f3af4a5e643a047d2ac68e0a8aa9217
konstantin <k.vasin@craftpromotion.ru>
2025-08-19 11:47:14 +0700

    save

---
+++ local/php_interface/migrations/OrmCreateFavoriteProduct20250819074602.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class OrmCreateFavoriteProduct20250819074602 extends Version
+{
+	protected $author = "admin";
+
+	protected $description = "";
+
+	protected $moduleVersion = "5.4.1";
+
+	public function up()
+	{
+		$sql = "CREATE TABLE favorite_product (
+    PRODUCT_ID INT NOT NULL,
+    USER_ID INT NOT NULL,
+    PRIMARY KEY (PRODUCT_ID, USER_ID)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+
+		global $DB;
+
+		$DB->Query($sql);
+	}
+
+	public function down()
+	{
+		//your code ...
+	}
+}
