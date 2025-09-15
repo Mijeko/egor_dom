922dff18e861fdd9d6bf81c836fe4aeb06a42fd1
konstantin <k.vasin@craftpromotion.ru>
2025-09-15 17:18:54 +0700

    save

--- local/markup/vuetify/components.d.ts
+++ local/markup/vuetify/components.d.ts
@@ -30,6 +30,7 @@ declare module 'vue' {
     Init: typeof import('./src/components/system/Init.vue')['default']
     InputDropdown: typeof import('./src/components/filter/part/InputDropdown.vue')['default']
     MainApartmentFilter: typeof import('./src/components/filter/MainApartmentFilter.vue')['default']
+    ManagerEdit: typeof import('./src/components/ManagerEdit.vue')['default']
     ManagerList: typeof import('./src/components/ManagerList.vue')['default']
     Map: typeof import('./src/components/Map.vue')['default']
     MaskInput: typeof import('./src/components/part/form/MaskInput.vue')['default']
---
+++ local/markup/vuetify/src/components/ManagerEdit.vue
@@ -0,0 +1,75 @@
+<script lang="ts">
+import {defineComponent, type PropType} from 'vue'
+import type BxUserDto from "@/dto/bitrix/BxUserDto.ts";
+import PhoneInput from "@/components/html/PhoneInput.vue";
+
+export default defineComponent({
+  name: "ManagerEdit",
+  components: {PhoneInput},
+  data: function () {
+    return {
+      isFormValid: false,
+      form: {
+        name: null,
+        lastName: null,
+        secondName: null,
+        phone: null,
+        email: null,
+      },
+      validate: {
+        name: [],
+        lastName: [],
+        secondName: [],
+        phone: [],
+        email: [],
+      },
+    };
+  },
+  props: {
+    manager: {
+      type: Object as PropType<BxUserDto>,
+      default: null
+    }
+  }
+})
+</script>
+
+<template>
+  <v-row>
+    <v-col cols="6">
+      <v-avatar
+        v-if="manager.avatar?.src"
+        :image="manager.avatar?.src"
+      />
+    </v-col>
+    <v-col cols="6">
+      <v-form>
+        <v-row>
+          <v-col cols="4">
+            <v-text-field v-model="form.lastName" :rules="validate.lastName" placeholder="Фамилия"/>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="form.name" :rules="validate.name" placeholder="Имя"/>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="form.secondName" :rules="validate.secondName" placeholder="Отчество"/>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="6">
+            <PhoneInput v-model="form.phone" :rules="validate.phone"/>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field v-model="form.email" :rules="validate.email" placeholder="E-Mail"/>
+          </v-col>
+        </v-row>
+
+      </v-form>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+
+</style>
