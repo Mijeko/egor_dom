916bd92ea206a9317c4e742717c1323c9cae9e4e
k.vasin <k.vasin@machineheads.ru>
2025-09-15 23:36:11 +0700

    save

---
+++ local/components/craft/agent.create/class.php
@@ -0,0 +1,31 @@
+<?php
+
+class CraftAgentCreateComponent extends \Craft\Core\Component\AjaxComponent
+{
+
+	function componentNamespace(): string
+	{
+		return 'craftAgentCreate';
+	}
+
+	protected function validate(array $postData): void
+	{
+	}
+
+	protected function work(array $formData): void
+	{
+	}
+
+	protected function modules(): ?array
+	{
+		return null;
+	}
+
+	protected function loadData(): void
+	{
+	}
+
+	public function loadServices(): void
+	{
+	}
+}
---
+++ local/components/craft/student.create/class.php
@@ -0,0 +1,31 @@
+<?php
+
+class CraftStudentCreateComponent extends \Craft\Core\Component\AjaxComponent
+{
+
+	function componentNamespace(): string
+	{
+		return 'craftStudentCreate';
+	}
+
+	protected function validate(array $postData): void
+	{
+	}
+
+	protected function work(array $formData): void
+	{
+	}
+
+	protected function modules(): ?array
+	{
+		return [];
+	}
+
+	protected function loadData(): void
+	{
+	}
+
+	public function loadServices(): void
+	{
+	}
+}
