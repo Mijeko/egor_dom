202b07120715c279122af58d3ddc3d5141e38c41
konstantin <k.vasin@craftpromotion.ru>
2025-10-10 13:08:30 +0700

    save

--- local/markup/vuetify/src/components/profile/developer/FeedLoad.vue
+++ local/markup/vuetify/src/components/profile/developer/FeedLoad.vue
@@ -12,7 +12,15 @@ export default defineComponent({
       default: [],
     }
   },
+  data: function () {
+    return {
+      lenSources: 2
+    };
+  },
   methods: {
+    incrementLenSource() {
+      this.lenSources += 1;
+    },
     submit() {
 
       let body: ManagerFeedUpdateRequestDto = {};
@@ -29,22 +37,25 @@ export default defineComponent({
 
 <template>
   <v-form @submit="submit">
-
-    <v-row>
-      <v-col>
-        <v-select
-          :items="source"
-          label="Источник"
-          item-value="value"
-          item-title="label"
-        />
-      </v-col>
-      <v-col>
-        <v-input label="Ссылка на источник"/>
-      </v-col>
-    </v-row>
-
-    <v-btn type="submit">Обновить</v-btn>
+    <v-card>
+      <v-card-text>
+        <v-row class="mb-4" v-for="ii in lenSources">
+          <v-col>
+            <v-select
+              :items="source"
+              label="Источник"
+              item-value="value"
+              item-title="label"
+            />
+          </v-col>
+          <v-col>
+            <v-input label="Ссылка на источник"/>
+          </v-col>
+        </v-row>
+        <v-btn type="button" @click="incrementLenSource">Еще</v-btn>
+        <v-btn type="submit">Обновить</v-btn>
+      </v-card-text>
+    </v-card>
   </v-form>
 </template>
 
