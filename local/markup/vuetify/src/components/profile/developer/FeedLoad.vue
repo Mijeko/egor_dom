ce7d5c1823041f7527d4690b4c1fa8c1e7468ff6
konstantin <k.vasin@craftpromotion.ru>
2025-10-13 10:39:18 +0700

    save

--- local/markup/vuetify/src/components/profile/developer/FeedLoad.vue
+++ local/markup/vuetify/src/components/profile/developer/FeedLoad.vue
@@ -39,6 +39,7 @@ export default defineComponent({
   <v-form @submit="submit">
     <v-card>
       <v-card-text>
+        <v-card-title>Источники квартир</v-card-title>
         <v-row class="mb-4" v-for="ii in lenSources">
           <v-col>
             <v-select
@@ -54,6 +55,22 @@ export default defineComponent({
         </v-row>
         <v-btn type="button" @click="incrementLenSource">Еще</v-btn>
         <v-btn type="submit">Обновить</v-btn>
+      </v-card-text>
+    </v-card>
+
+    <v-card>
+      <v-card-text>
+        <v-card-title>Способ получения заявки</v-card-title>
+        <v-checkbox name="channelLead" label="Tg"></v-checkbox>
+        <v-checkbox name="channelLead" label="Email"></v-checkbox>
+        <v-checkbox name="channelLead" label="Call"></v-checkbox>
+      </v-card-text>
+    </v-card>
+
+    <v-card>
+      <v-card-text>
+        <v-card-title>Время удержания брони</v-card-title>
+        <v-text-field name="timeoutBron" label="Время удержания брони, в часах"/>
       </v-card-text>
     </v-card>
   </v-form>
