0edc461d3a209ada77445c0dee51ae02ffd918b8
konstantin <k.vasin@craftpromotion.ru>
2025-10-16 16:37:26 +0700

    save

--- local/php_interface/lib/Craft/DDD/Developers/Application/Dto/DeveloperSettingsUpdateDto.php
+++ local/php_interface/lib/Craft/DDD/Developers/Application/Dto/DeveloperSettingsUpdateDto.php
@@ -5,11 +5,16 @@ namespace Craft\DDD\Developers\Application\Dto;
 final class DeveloperSettingsUpdateDto
 {
 	public function __construct(
-		public int    $developerId,
-		public ?array $sources = null,
-		public ?int   $timeoutBron = null,
-		public ?int   $timePay = null,
-		public ?array $channelLead = null,
+		public int     $developerId,
+		public ?array  $sources = null,
+		public ?int    $timeoutBron = null,
+		public ?int    $timePay = null,
+		public ?array  $channelLead = null,
+		public ?string $tgId = null,
+		public ?string $email = null,
+		public ?string $maxReservHours = null,
+		public ?string $timeToPayments = null,
+		public ?array  $feedList = null,
 	)
 	{
 	}
--- local/php_interface/lib/Craft/DDD/Developers/Application/UseCase/DeveloperSettingsUpdateUseCase.php
+++ local/php_interface/lib/Craft/DDD/Developers/Application/UseCase/DeveloperSettingsUpdateUseCase.php
@@ -4,6 +4,10 @@ namespace Craft\DDD\Developers\Application\UseCase;
 
 use Craft\DDD\Developers\Application\Dto\DeveloperSettingsUpdateDto;
 use Craft\DDD\Developers\Domain\Repository\DeveloperRepositoryInterface;
+use Craft\DDD\Developers\Domain\ValueObject\DeveloperSettingsValueObject;
+use Craft\DDD\Shared\Domain\ValueObject\ChannelEmailValueObject;
+use Craft\DDD\Shared\Domain\ValueObject\ChannelTgValueObject;
+use Craft\DDD\Shared\Domain\ValueObject\ContactChannelInterface;
 
 class DeveloperSettingsUpdateUseCase
 {
@@ -22,10 +26,36 @@ class DeveloperSettingsUpdateUseCase
 			throw new \Exception("Developer with ID {$paramsDto->developerId} not found");
 		}
 
-		$settingsValueObject = $developer->getSettings();
+		$currentSettingsValueObject = $developer->getSettings();
 
 
-		$developer->updateSettings($settingsValueObject);
+		$newSettingsValueObject = new DeveloperSettingsValueObject(
+			$paramsDto->channelLead ? array_map(function(string $key) use ($paramsDto) {
+
+				switch($key)
+				{
+					case 'email':
+						return new ChannelEmailValueObject(
+							'Y',
+							$paramsDto->email
+						);
+					case 'tg':
+						return new ChannelTgValueObject(
+							'Y',
+							$paramsDto->tgId,
+						);
+						break;
+					default:
+						return null;
+				}
+
+			}, $paramsDto->channelLead) : $currentSettingsValueObject->getLeadChannelList(),
+			$paramsDto->feedList ?? null,
+			$paramsDto->maxReservHours ?? null,
+			$paramsDto->timeToPayments ?? null,
+		);
+
+		$developer->updateSettings($newSettingsValueObject);
 
 		$developer = $this->developerRepository->update($developer);
 	}
