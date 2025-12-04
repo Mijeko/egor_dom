8b6d42332c9bffb02c068aef22dc6cbd45e32ed5
konstantin <k.vasin@craftpromotion.ru>
2025-12-04 16:19:39 +0700

    save

--- local/markup/abn/src/pages/profile/home.vue
+++ local/markup/abn/src/pages/profile/home.vue
@@ -19,18 +19,78 @@ export default defineComponent({
     <div class="profile-main-card">
       <div class="profile-main-card-title">Информация о сделках</div>
       <div class="profile-main-card-wrap">
+
+        <div class="personal-reward">
+          <div class="personal-reward-title">
+            Вознаграждение: 4%
+          </div>
+          <div class="personal-reward-body">
+            <div class="personal-reward-chars">
+
+              <div class="personal-reward-chars-row">
+                <div class="personal-reward-chars-key">Кол-во сделок:</div>
+                <div class="personal-reward-chars-value">0</div>
+              </div>
+
+              <div class="personal-reward-chars-row">
+                <div class="personal-reward-chars-key">Оборот:</div>
+                <div class="personal-reward-chars-value">0</div>
+              </div>
+
+              <div class="personal-reward-chars-row">
+                <div class="personal-reward-chars-key">К получению:</div>
+                <div class="personal-reward-chars-value">0</div>
+              </div>
+
+            </div>
+          </div>
+
+          <Button>Подробнее</Button>
+        </div>
+
       </div>
     </div>
+
 
     <div class="profile-main-card">
       <div class="profile-main-card-title">Реферальная система</div>
       <div class="profile-main-card-wrap">
+        <div class="referral-data">
+          <div class="referral-data-link">
+            <div class="referral-data-link__title">
+              Ссылка-приглашение
+            </div>
+
+            <div class="referral-data-link__source">
+              http://abn.dev-kvsn.ru/ref/68ca83b5e9c52
+            </div>
+          </div>
+
+          <div class="referral-data-char">
+            <div class="referral-data-char-row">
+              <div class="referral-data-char-key">Приглашено</div>
+              <div class="referral-data-char-value">0</div>
+            </div>
+            <div class="referral-data-char-row">
+              <div class="referral-data-char-key">Вознаграждение</div>
+              <div class="referral-data-char-value">0 руб</div>
+            </div>
+          </div>
+
+          <Button>Скопировать</Button>
+        </div>
       </div>
     </div>
+
 
     <div class="profile-main-card">
       <div class="profile-main-card-title">Просмотренные</div>
       <div class="profile-main-card-wrap">
+        <div class="visited-list">
+          <a href="" class="visited-item">Дом в мичурино</a>
+          <a href="" class="visited-item">Дом в мичурино</a>
+          <a href="" class="visited-item">Дом в мичурино</a>
+        </div>
       </div>
     </div>
 
@@ -49,6 +109,8 @@ export default defineComponent({
 
   &-cards {
     display: flex;
+    align-items: stretch;
+    justify-content: flex-start;
     gap: 15px;
   }
 
@@ -76,6 +138,124 @@ export default defineComponent({
       text-align: center;
       color: $gray-color;
     }
+  }
+}
+
+.personal-reward {
+
+  &-title {
+    margin-bottom: 26px;
+    border-radius: 10px;
+    background: $personal-reward-bg;
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 100%;
+    color: #fff;
+    padding: 5px 40px;
+  }
+
+  &-body {
+    margin-bottom: 30px;
+  }
+
+  &-chars {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    &-row {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+    }
+
+    &-key {
+      font-family: var(--font-family);
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 250%;
+      color: $gray-color;
+
+    }
+
+    &-value {
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 250%;
+      text-align: right;
+      color: $bo-color-name;
+    }
+  }
+}
+
+.referral-data {
+  &-link {
+    margin-bottom: 20px;
+
+    &__title {
+      font-family: var(--font-family);
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 100%;
+      color: $ref-title-color;
+      margin-bottom: 10px;
+    }
+
+    &__source {
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 140%;
+      color: $personal-reward-bg;
+    }
+  }
+
+  &-char {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+
+    &-row {
+      display: flex;
+      align-items: flex-start;
+      justify-content: space-between;
+    }
+
+    &-key {
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 250%;
+      color: $gray-color;
+    }
+
+    &-value {
+      font-family: var(--font-family);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 250%;
+      text-align: right;
+      color: $bo-color-name;
+    }
+  }
+}
+
+.visited {
+  &-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  &-item {
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 250%;
+    color: $bo-color-name;
   }
 }
 </style>
